bac541849a125fc4f8a1294bfec80e9b8091e0ae
Elikem Medehou <elikem@Latitude-7420-81920f50.localdomain>
2024-04-10 23:59:30 +0100

    (remove) meta tags seo

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,10 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart';
-import 'package:meta_seo/meta_seo.dart';
 import 'package:seo/seo.dart';
 
 void main() {
   usePathUrlStrategy();
-
-  if (kIsWeb) {
-    MetaSEO().config();
-  }
 
   runApp(const MyApp());
 }
