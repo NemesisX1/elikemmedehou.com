424d7b7cff6ab593176a0834a569029d654ac69d
Elikem Medehou <elikem@Latitude-7420-81920f50.localdomain>
2024-04-11 11:55:40 +0200

    (remove) meta tags seo

--- lib/pages/home/home_mobile.dart
+++ lib/pages/home/home_mobile.dart
@@ -1,5 +1,4 @@
 import 'package:elikemmedehou/shared/colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
